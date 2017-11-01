17c00ae1a7f70a7ae14d195222ceabe5e4e11854
Fernando Alves <fernando.alves@fluig.com>
2017-11-01 17:55:10 -0200

    test update

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require "login/loginheader.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>High Digital</title>
+  </head>
+  <body>
+    Hello World !!!
+  </body>
+</html>
